36814582d07428597f71551e03e6fef607e923f8
KrouriB <brice.krouri@gmail.com>
2023-02-10 16:53:46 +0100

    add class reservation, ajoutReservation  construc

---
+++ Reservation.php
@@ -0,0 +1,62 @@
+<?php
+
+class Reservation
+{
+    private DateTime $datedebut;
+    private DateTime $datefin;
+    private Client $leclient;
+    private Chambre $lachambre;
+
+    public function __construct(string $ladatedebut, string $ladatefin, Client $leclient, Chambre $lachambre)
+    {
+        $this->lachambre = $lachambre;
+        if ($this->lachambre->get_etatChambre() == false)
+        {
+            echo "la chambre est deja occupé";
+        }
+        else
+        {
+            $this->datedebut = new DateTime($ladatedebut);
+            $this->datefin = new DateTime($ladatefin);
+            $this->leclient = $leclient;
+            $this->leclient->AjoutReservation($this); // future fonction qui stockera les objet reservation d'un objet client
+            $this->lachambre->set_etatChambre(false); //mettre l'etat de la chambre comme occupé via une fonction set
+            $this->lachambre->get_hotel()->AjoutReservation($this); // future fonction qui stockera les objet reservation d'un objet hotel par rapport a ces chambre
+        } 
+    }
+    
+    public function set_datedebut(string $valeur)
+	{
+		$this->datedebut = new DateTime($valeur);
+	}
+	public function get_datedebut()
+	{
+		return $this->datedebut;
+	}
+    public function set_datefin(string $valeur)
+	{
+		$this->datefin = new DateTime($valeur);
+	}
+	public function get_datefin()
+	{
+		return $this->datefin;
+	}
+    public function set_leclient(Client $valeur)
+    {
+        return $this->leclient = $valeur;
+    }
+    public function get_leclient()
+    {
+        return $this->leclient;
+    }
+    public function set_lachambre(Chambre $valeur)
+    {
+        return $this->lachambre = $valeur;
+    }
+    public function get_lachambre()
+    {
+        return $this->lachambre;
+    }
+}
+
+?>
